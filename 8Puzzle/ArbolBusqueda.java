0c6f28a25f5b8732bc96425d6e40a319ab0b34bf
Ray0beso <19170636@itculiacan.edu.mx>
2023-03-01 19:15:33 -0600

    Métodos con heurística agregados

--- 8Puzzle/ArbolBusqueda.java
+++ 8Puzzle/ArbolBusqueda.java
@@ -1,13 +1,15 @@
 
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.PriorityQueue;
 
 public class ArbolBusqueda {
     
-	private Nodo raiz, nObjetivo;
+    private Nodo raiz, nObjetivo;
     private String objetivo;
     private String tipoDeBusqueda;
     
@@ -66,6 +68,45 @@ public class ArbolBusqueda {
             estadoActual = nodoActual.getEstado();
         }
         nObjetivo = nodoActual;
+    }
+    
+  //Busqueda por Anchura
+    public void busquedaPorHeuristica(int metodoHeuristico) {
+    	
+    	if(nObjetivo!=null) LimpiarArbol();
+    	
+    	tipoDeBusqueda = "Heurística "+metodoHeuristico;
+    	raiz.setHeuristica(metodoHeuristico);
+    	
+        Nodo nodoActual = raiz;
+        String estadoActual = raiz.getEstado();
+        Collection<String> estadosVisitados = new LinkedList<>();
+        PriorityQueue<Nodo> cola = new PriorityQueue<>(comparator());
+        Collection<Nodo> hijos;
+        
+        while(!estadoActual.equals(objetivo)) {
+            estadosVisitados.add(estadoActual);
+            hijos = nodoActual.getHijos();
+            for (Nodo hijo : hijos) {
+                if(!estadosVisitados.contains(hijo.getEstado())) {
+                    cola.add(hijo);
+                    
+                }
+            }
+            nodoActual = cola.poll();
+            estadoActual = nodoActual.getEstado();
+        }
+        nObjetivo = nodoActual;
+    }
+    
+    public Comparator<Nodo> comparator() {
+        Comparator<Nodo> c = new Comparator<Nodo>() {
+            @Override
+            public int compare(Nodo n, Nodo n2) {
+            	return n.ValorHeuristico(objetivo);
+            }
+        };
+        return c;
     }
 
     private void LimpiarArbol() {
--- 8Puzzle/Puzzle8.java
+++ 8Puzzle/Puzzle8.java
@@ -2,27 +2,33 @@
 
 public class Puzzle8 {
 
-//	public static String estadoInicial = "12578 346";
-    public static String estadoInicial = "41275386 ";
-    public static String estadoFinal = "12345678 ";
-    
     public static void main(String[] args) {
+    	
+//    		String estadoInicial = "12578 346";
+//      	String estadoInicial = "41275386 ";
+    	String estadoInicial = "41275386 ";
+    	String estadoFinal   = "12345678 ";
+    	
         
         //Instanciar el arbol
         ArbolBusqueda a = new ArbolBusqueda(estadoInicial, estadoFinal);
-        
-        //Ejecuta la busqueda
+       
+        //Realiza cada una de las busquedas y las imprime
         a.busquedaPorProfundidad();
-        
-        //Imprime arbol
         a.imprimirSolucion();
         
-        // Cambia nodos del arbol por la busqueda en anchura
         a.busquedaPorAnchura();
-
-        //Imprime arbol
         a.imprimirSolucion();
         
+        a.busquedaPorHeuristica(1);
+        a.imprimirSolucion();
+        
+        a.busquedaPorHeuristica(2);
+        a.imprimirSolucion();
+        
+        a.busquedaPorHeuristica(3);
+        a.imprimirSolucion();
+    
     }
     
 }
